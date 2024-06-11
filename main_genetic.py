cb685f7d070694865a8380e2967c84dd18656588
VaruN-dev-dev <sanatoo.varun666@gmail.com>
2024-06-11 16:16:53 +0530

    removed pygame from training part, can now run on colab/kaggle

--- main_genetic.py
+++ main_genetic.py
@@ -1,4 +1,4 @@
-import pygame
+# import pygame
 import os
 import sys
 from collections import namedtuple
@@ -16,44 +16,45 @@ class Game:
     def __init__(self,
                  n_agents=1000,
                  total_games=1000):
-        pygame.init()
+        # pygame.init()
 
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption("Snake AI genetic Algorithm")
+        # self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        # pygame.display.set_caption("Snake AI genetic Algorithm")
 
-        self.clock = pygame.time.Clock()
-        self.font = pygame.font.Font("arial.ttf", 25)
+        # self.clock = pygame.time.Clock()
+        # self.font = pygame.font.Font("arial.ttf", 25)
         self.n_agents = n_agents
         self.total_games = total_games
         self.agents = Agents(w=WIDTH,
                              h=HEIGHT,
                              n_agents=n_agents,
-                             screen=self.screen,
-                             font=self.font)
+                             screen=None,
+                             font=None)
 
-    def display_blocks(self):
-        """
-        Displays blocks in the game screen
-        """
-        for x in range(0, WIDTH, BLOCK_SIZE):
-            for y in range(0, HEIGHT, BLOCK_SIZE):
-                pygame.draw.rect(self.screen,
-                                 Color.PURPLE.value,
-                                 pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE))
+    # def display_blocks(self):
+    #     """
+    #     Displays blocks in the game screen
+    #     """
+    #     import pygame
+    #     for x in range(0, WIDTH, BLOCK_SIZE):
+    #         for y in range(0, HEIGHT, BLOCK_SIZE):
+    #             pygame.draw.rect(self.screen,
+    #                              Color.PURPLE.value,
+    #                              pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE))
 
-                # This is the width for inner rectangle.
-                z = 0.2
-                pygame.draw.rect(self.screen,
-                                 Color.BLACK.value,
-                                 pygame.Rect(x+z, y+z, BLOCK_SIZE - 2*z, BLOCK_SIZE - 2*z))
+    #             # This is the width for inner rectangle.
+    #             z = 0.2
+    #             pygame.draw.rect(self.screen,
+    #                              Color.BLACK.value,
+    #                              pygame.Rect(x+z, y+z, BLOCK_SIZE - 2*z, BLOCK_SIZE - 2*z))
 
-    def extra_display(self, gen):
-        """
-        Displays extra information
-        """
-        text = self.font.render(
-            "GEN: " + str(gen), True, Color.WHITE.value)
-        self.screen.blit(text, [WIDTH - 7 * BLOCK_SIZE, 0])
+    # def extra_display(self, gen):
+    #     """
+    #     Displays extra information
+    #     """
+    #     text = self.font.render(
+    #         "GEN: " + str(gen), True, Color.WHITE.value)
+    #     self.screen.blit(text, [WIDTH - 7 * BLOCK_SIZE, 0])
 
     def save_best_agent(self, game_no):
         """
@@ -149,14 +150,14 @@ class Game:
                         games_played_by_curr_gen += 1
                         break
 
-                    self.screen.fill(Color.BLACK.value)
+                    # self.screen.fill(Color.BLACK.value)
                     # self.display_blocks()
                     # self.extra_display(self.agents.gen)
 
-                    for event in pygame.event.get():
-                        if event.type == pygame.QUIT:
-                            pygame.quit()
-                            quit()
+                    # for event in pygame.event.get():
+                    #     if event.type == pygame.QUIT:
+                    #         pygame.quit()
+                    #         quit()
 
                     self.current_agent.get_state()
                     self.current_agent.take_action()
@@ -167,7 +168,7 @@ class Game:
 
                     self.agents.update_steps()
 
-                    pygame.display.flip()
+                    # pygame.display.flip()
 
                     # For maximum speed omit this line
                     # self.clock.tick(FPS)
@@ -182,39 +183,39 @@ class Game:
         #     print()
         __print_scores()
 
-    def temp(self, flag=0):
-        if flag == 0:
+    # def temp(self, flag=0):
+    #     if flag == 0:
 
-            flag = 1
-            from _agent import Agent
-            from snake import Snake
+    #         flag = 1
+    #         from _agent import Agent
+    #         from snake import Snake
 
-            self.snake = Snake(w=WIDTH, h=HEIGHT)
-            self.snake.display = self.screen
-            self.snake.font = self.font
+    #         self.snake = Snake(w=WIDTH, h=HEIGHT)
+    #         self.snake.display = self.screen
+    #         self.snake.font = self.font
 
-            self.agent = Agent(snake=self.snake)
+    #         self.agent = Agent(snake=self.snake)
 
-        while True:
-            if self.snake.game_over:
-                self.snake.reset()
-            self.screen.fill(Color.BLACK.value)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
+    #     while True:
+    #         if self.snake.game_over:
+    #             self.snake.reset()
+    #         self.screen.fill(Color.BLACK.value)
+    #         for event in pygame.event.get():
+    #             if event.type == pygame.QUIT:
+    #                 pygame.quit()
+    #                 quit()
 
-            self.display_blocks()
+    #         self.display_blocks()
 
-            self.agent.get_state()
-            self.agent.take_action()
+    #         self.agent.get_state()
+    #         self.agent.take_action()
 
-            self.snake.update()
-            self.snake.render()
-            pygame.display.flip()
-            self.clock.tick(FPS)
+    #         self.snake.update()
+    #         self.snake.render()
+    #         pygame.display.flip()
+    #         self.clock.tick(FPS)
 
-        print(f"Final Score: {self.snake.score}")
+        # print(f"Final Score: {self.snake.score}")
 
 
 Game().play()
